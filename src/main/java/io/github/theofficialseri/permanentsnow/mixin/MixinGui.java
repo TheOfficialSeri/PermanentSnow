85fdeab7c62810f6e4411f2d4b6bd12ec65bacc8
TheOfficialSeri <theofficialseri@gmail.com>
2026-03-25 10:33:50 +0100

    Update to 26.1

--- src/main/java/io/github/theofficialseri/permanentsnow/mixin/MixinGui.java
+++ src/main/java/io/github/theofficialseri/permanentsnow/mixin/MixinGui.java
@@ -9,15 +9,15 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Gui.class)
 public abstract class MixinGui {
-    @Redirect(method = "renderCameraOverlays(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/DeltaTracker;)V", at = @At(value = "INVOKE", target
-            = "Lnet/minecraft/client/player/LocalPlayer;getTicksFrozen ()I"))
+    @Redirect(method = "extractCameraOverlays(Lnet/minecraft/client/gui/GuiGraphicsExtractor;Lnet/minecraft/client/DeltaTracker;)V", at = @At(value = "INVOKE"
+            , target = "Lnet/minecraft/client/player/LocalPlayer;getTicksFrozen ()I"))
     private static int getTicksFrozen(LocalPlayer instance) {
         int ticksFrozen = instance.getTicksFrozen();
         return Configs.powderSnowOverlayOpacity > 0 && ticksFrozen <= 0 ? 1 : ticksFrozen;
     }
 
-    @Redirect(method = "renderCameraOverlays(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/DeltaTracker;)V", at = @At(value = "INVOKE", target
-            = "net/minecraft/client/player/LocalPlayer.getPercentFrozen ()F"))
+    @Redirect(method = "extractCameraOverlays(Lnet/minecraft/client/gui/GuiGraphicsExtractor;Lnet/minecraft/client/DeltaTracker;)V", at = @At(value = "INVOKE"
+            , target = "net/minecraft/client/player/LocalPlayer.getPercentFrozen ()F"))
     private static float getPercentFrozen(LocalPlayer instance) {
         return Configs.powderSnowOverlayOpacity > 0 && instance.getTicksFrozen() <= 0 ? Configs.powderSnowOverlayOpacity : instance.getPercentFrozen();
     }
